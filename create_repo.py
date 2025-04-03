56693dfdef065ff29175a4d742d52c9ae07430ed
yuta15 <yxyi8udm1eb1us6aea7s@gmail.com>
2025-04-03 21:19:37 +0900

    first commit

---
+++ create_repo.py
@@ -0,0 +1,36 @@
+from github import Github
+import os
+
+# 環境変数の読み込み
+GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
+# FILE_PATH = 
+
+
+def create_repo():
+    """リポジトリを作成する関数"""
+    pass
+
+
+def create_dirs():
+    """追加するdirectoryを作成する"""
+    pass
+
+
+def create_branch():
+    """リポジトリに既定のブランチを作成する"""
+    pass
+
+
+def set_branch_rule():
+    """リポジトリのルールを設定する"""
+    pass
+
+
+
+
+
+if __name__ == '__main__':
+    create_repo()
+    create_dirs()
+    create_branch()
+    set_branch_rule()
