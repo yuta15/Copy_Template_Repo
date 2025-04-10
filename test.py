7eed8778a4079aa20b3dadcddb5cf55d2b16cd73
yuta15 <yxyi8udm1eb1us6aea7s@gmail.com>
2025-04-10 21:46:39 +0900

    bash -> ./へ修正

--- test.py
+++ test.py
@@ -1,17 +1,4 @@
-import os
-import yaml
-import json
-from github import Github
-
-from mods.read_params import read_params
-from mods.setting import setting
-from mods.set_docker_compose import set_docker_compose
+#!/usr/bin/env python
 
 
-g = Github(setting.GITHUB_TOKEN)
-user = g.get_user()
-repo = user.get_repo(
-    'test6'
-)
-
-print(repo.html_url)
+print('test.py')
