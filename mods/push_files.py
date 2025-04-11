8192a05a1c62f50a86e590a5cf8db465fbfa49d9
yuta15 <yxyi8udm1eb1us6aea7s@gmail.com>
2025-04-11 20:51:20 +0900

    test-commit

--- mods/push_files.py
+++ mods/push_files.py
@@ -13,19 +13,33 @@ def push_files(repo):
         # リポジトリ変更
         remote_repo_url = f'https://github.com/{setting.USERNAME}:{setting.GITHUB_TOKEN}/{repo.name}.git'
         os.chdir(setting.ROOT_DIR+"/templates")
+        
+        logger.info('Initializing repository...')
         repository = git.Repo.init(f'{setting.ROOT_DIR}/templates')
+        
+        logger.info('create remote...')
         remote = repository.create_remote(name="origin", url=remote_repo_url)
         index = repository.index
         
-        remote.pull('main')
-        logger.info(repository.branches)
         act_branch = repository.active_branch.name
         logger.info(f"Active branch: {act_branch}")
+        
+        logger.info('pulling...')
+        remote.pull(act_branch)
+        
+        logger.info(repository.branches)
+        logger.info('Adding files...')
         index.add(["."])
+        
+        logger.info('committing...')
         index.commit("Add files")
-        remote.push('master')
+        
+        logger.info('pushing...')
+        remote.push(act_branch)
+        
     except Exception as e:
         logger.error(f"Push failed: {e}")
         raise
+    
     else:
         logger.info("Push completed")
